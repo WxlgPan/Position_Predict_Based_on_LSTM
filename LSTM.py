513ae0a890e4602cb3172712a0a2de47d1d3be93
WxlgPan <46819933+WxlgPan@users.noreply.github.com>
2020-08-12 20:14:32 -0700

    Add files via upload

---
+++ LSTM.py
@@ -0,0 +1,161 @@
+import numpy as np
+from tensorflow import keras
+#from keras.layers.core import Dense, Activation, Dropout
+#from keras.layers import LSTM
+#from keras.models import Sequential, load_model
+#from keras.callbacks import Callback
+#import keras.backend.tensorflow_backend as KTF
+import tensorflow as tf
+import pandas as pd
+import os
+#import keras.callbacks
+import matplotlib.pyplot as plt
+
+class LSTM_createModel:
+    def __init__(self):
+        # 设定为自增长
+        os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+        self.config = tf.ConfigProto()
+        self.config.gpu_options.allow_growth = True
+        self.session = tf.Session(config=self.config)
+        keras.backend.set_session(self.session)
+
+
+    def create_dataset(self,data, n_predictions, n_next):
+        '''
+        对数据进行处理
+        '''
+        dim = data.shape[1]
+        train_X, train_Y = [], []
+        for i in range(data.shape[0] - n_predictions - n_next - 1):
+            a = data[i:(i + n_predictions), :]
+            train_X.append(a)
+            tempb = data[(i + n_predictions):(i + n_predictions + n_next), :]
+            b = []
+            for j in range(len(tempb)):
+                for k in range(dim):
+                    b.append(tempb[j, k])
+            train_Y.append(b)
+        train_X = np.array(train_X, dtype='float64')
+        train_Y = np.array(train_Y, dtype='float64')
+
+        test_X, test_Y = [], []
+        i = data.shape[0] - n_predictions - n_next - 1
+        a = data[i:(i + n_predictions), :]
+        test_X.append(a)
+        tempb = data[(i + n_predictions):(i + n_predictions + n_next), :]
+        b = []
+        for j in range(len(tempb)):
+            for k in range(dim):
+                b.append(tempb[j, k])
+        test_Y.append(b)
+        test_X = np.array(test_X, dtype='float64')
+        test_Y = np.array(test_Y, dtype='float64')
+
+        return train_X, train_Y, test_X, test_Y
+
+
+    def NormalizeMult(self,data, set_range):
+        '''
+        返回归一化后的数据和最大最小值
+        '''
+        normalize = np.arange(2 * data.shape[1], dtype='float64')
+        normalize = normalize.reshape(data.shape[1], 2)
+
+        for i in range(0, data.shape[1]):
+            if set_range == True:
+                list = data[:, i]
+                listlow, listhigh = np.percentile(list, [0, 100])
+            else:
+                if i == 0:
+                    listlow = -90
+                    listhigh = 90
+                else:
+                    listlow = -180
+                    listhigh = 180
+
+            normalize[i, 0] = listlow
+            normalize[i, 1] = listhigh
+
+            delta = listhigh - listlow
+            if delta != 0:
+                for j in range(0, data.shape[0]):
+                    data[j, i] = (data[j, i] - listlow) / delta
+
+        return data, normalize
+
+
+    def trainModel(self,train_X, train_Y):
+        '''
+        trainX，trainY: 训练LSTM模型所需要的数据
+        '''
+        model = keras.models.Sequential()
+        model.add(keras.layers.LSTM(
+            120,
+            input_shape=(train_X.shape[1], train_X.shape[2]),
+            return_sequences=True))
+        model.add(keras.layers.Dropout(0.3))
+
+        model.add(keras.layers.LSTM(
+            120,
+            return_sequences=False))
+        model.add(keras.layers.Dropout(0.3))
+
+        model.add(keras.layers.Dense(
+            train_Y.shape[1]))
+        model.add(keras.layers.Activation("relu"))
+
+        model.compile(loss='mse', optimizer='adam', metrics=['acc'])
+        model.fit(train_X, train_Y, epochs=100, batch_size=64, verbose=1)
+        model.summary()
+
+        return model
+
+    def train_and_save(self,userid,train_num=6,pred_num=1,draw_pic=True):
+        per_num = pred_num
+        # set_range = False
+        set_range = True
+
+        # 读入时间序列的文件数据
+        data = np.empty((0, 2))
+        #print(data.shape)
+        for i, j, filenamelst in os.walk('./Geolife Trajectories 1.3/Data/' + userid + '/Trajectory/'):
+            for filename in filenamelst:
+                tmpdata = pd.read_csv('./Geolife Trajectories 1.3/Data/' + userid + '/Trajectory/' + str(filename),
+                                      sep=',',
+                                      header=6).iloc[:,
+                          0:2].values
+                data = np.concatenate((data, tmpdata), axis=0)
+        print("样本数：{0}，维度：{1}".format(data.shape[0], data.shape[1]))
+        print(data)
+        if draw_pic:
+            # 画样本数据库
+            plt.scatter(data[:, 1], data[:, 0], c='b', marker='o', label='traj_A')
+            plt.legend(loc='upper left')
+            plt.grid()
+            plt.show()
+
+        # 归一化
+        data, normalize = self.NormalizeMult(data, set_range)
+        print(normalize)
+
+        # 生成训练数据
+        train_X, train_Y, test_X, test_Y = self.create_dataset(data, train_num, per_num)
+        print("x\n", train_X.shape)
+        print("y\n", train_Y.shape)
+
+        # 训练模型
+        model = self.trainModel(train_X, train_Y)
+        loss, acc = model.evaluate(train_X, train_Y, verbose=2)
+        print('Loss : {}, Accuracy: {}'.format(loss, acc * 100))
+
+        # 保存模型
+        if not os.path.exists('npy'):
+            os.mkdir(os.getcwd() + '\\npy')
+        np.save("./npy/"+userid+"_traj_model_trueNorm.npy", normalize)
+        model.save("./model/"+userid+"_traj_model.h5")
+
+if __name__ == "__main__":
+    newmodel = LSTM_createModel()
+    newmodel.train_and_save(userid='000',train_num=6,pred_num=1)
+    
---
+++ LSTMPredict.py
@@ -0,0 +1,129 @@
+import numpy as np
+from tensorflow import keras
+import tensorflow as tf
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import copy
+from math import sin, asin, cos, radians, fabs, sqrt
+
+class Predict_via_LSTM_model:
+    def __init__(self):
+        # 设定为自增长
+        self.config = tf.ConfigProto()
+        self.config.gpu_options.allow_growth = True
+        self.session = tf.Session(config=self.config)
+        keras.backend.set_session(self.session)
+
+        self.EARTH_RADIUS = 6371  # 地球平均半径，6371km
+
+
+    def rmse(self,predictions, targets):
+        return np.sqrt(((predictions - targets) ** 2).mean())
+
+
+    def mse(self,predictions, targets):
+        return ((predictions - targets) ** 2).mean()
+
+
+    def reshape_y_hat(self,y_hat, dim):
+        re_y = []
+        i = 0
+        while i < len(y_hat):
+            tmp = []
+            for j in range(dim):
+                tmp.append(y_hat[i + j])
+            i = i + dim
+            re_y.append(tmp)
+        re_y = np.array(re_y, dtype='float64')
+        return re_y
+
+
+    # 多维反归一化
+    def FNormalizeMult(self,data, normalize):
+        data = np.array(data, dtype='float64')
+        # 列
+        for i in range(0, data.shape[1]):
+            listlow = normalize[i, 0]
+            listhigh = normalize[i, 1]
+            delta = listhigh - listlow
+            print("listlow, listhigh, delta", listlow, listhigh, delta)
+            # 行
+            if delta != 0:
+                for j in range(0, data.shape[0]):
+                    data[j, i] = data[j, i] * delta + listlow
+
+        return data
+
+
+    # 使用训练数据的归一化
+    def NormalizeMultUseData(self,data, normalize):
+        for i in range(0, data.shape[1]):
+
+            listlow = normalize[i, 0]
+            listhigh = normalize[i, 1]
+            delta = listhigh - listlow
+
+            if delta != 0:
+                for j in range(0, data.shape[0]):
+                    data[j, i] = (data[j, i] - listlow) / delta
+
+        return data
+
+    # 计算两个经纬度之间的直线距离
+    def hav(self,theta):
+        s = sin(theta / 2)
+        return s * s
+
+
+    def get_distance_hav(self,lat0, lng0, lat1, lng1):
+        # "用haversine公式计算球面两点间的距离。"
+        # 经纬度转换成弧度
+        lat0 = radians(lat0)
+        lat1 = radians(lat1)
+        lng0 = radians(lng0)
+        lng1 = radians(lng1)
+
+        dlng = fabs(lng0 - lng1)
+        dlat = fabs(lat0 - lat1)
+        h = self.hav(dlat) + cos(lat0) * cos(lat1) * self.hav(dlng)
+        distance = 2 * self.EARTH_RADIUS * asin(sqrt(h))
+        return distance
+
+    def predict_and_draw(self,userid, test_num=6, pred_num=1,draw_pic=True):
+        per_num = pred_num
+        data_all = pd.read_csv('./Geolife Trajectories 1.3/Data/'+userid+'/Trajectory/20081023025304.plt', sep=',',header=6)
+        data_all = data_all.iloc[-2 * (test_num + per_num):-1 * (test_num + per_num),0:2].values
+        print(data_all)
+        data_all.dtype = 'float64'
+
+        data = copy.deepcopy(data_all[:-per_num, :])
+        y = data_all[-per_num:, :]
+
+        # #归一化
+        normalize = np.load("./npy/traj_model_trueNorm.npy")
+        data = self.NormalizeMultUseData(data, normalize)
+
+        model = keras.models.load_model("./model/traj_model_120.h5")
+        test_X = data.reshape(1, data.shape[0], data.shape[1])
+        y_hat = model.predict(test_X)
+        y_hat = y_hat.reshape(y_hat.shape[1])
+        y_hat = self.reshape_y_hat(y_hat, 2)
+
+        # 反归一化
+        y_hat = self.FNormalizeMult(y_hat, normalize)
+        print("predict: {0}\ntrue：{1}".format(y_hat, y))
+        print('预测均方误差：', self.mse(y_hat, y))
+        print('预测直线距离：{:.4f} KM'.format(self.get_distance_hav(y_hat[0, 0], y_hat[0, 1], y[0, 0], y[0, 1])))
+        if draw_pic:
+            # 画测试样本数据库
+            p1 = plt.scatter(data_all[:-per_num, 1], data_all[:-per_num, 0], c='b', marker='o', label='traj_A')
+            p2 = plt.scatter(y_hat[:, 1], y_hat[:, 0], c='r', marker='o', label='pre')
+            p3 = plt.scatter(y[:, 1], y[:, 0], c='g', marker='o', label='pre_true')
+            plt.legend(loc='upper left')
+            plt.grid()
+            plt.show()
+
+if __name__ == '__main__':
+    trained_model = Predict_via_LSTM_model()
+    trained_model.predict_and_draw(userid='000',test_num=6,pred_num=1)
